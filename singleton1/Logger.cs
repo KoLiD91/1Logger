3f7d68facbdffc8a11ebd6d8ec80959ed7de1ca5
KoLiD91 <kolid1991@gmail.com>
2025-01-31 18:57:17 +0100

    udapte

---
+++ singleton1/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton1
+{
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _instance =
+            new Lazy<Logger>(() => new Logger());
+
+        private readonly List<string> _messages;
+
+        public static Logger Instance => _instance.Value;
+
+        private Logger()
+        {
+            _messages = new List<string>();
+        }
+
+        public void Log(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentException("Message cannot be empty", nameof(message));
+
+            _messages.Add(message);
+            Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
+        }
+
+        public IReadOnlyList<string> Messages => _messages.AsReadOnly();
+    }
+}
--- singleton1/Program.cs
+++ singleton1/Program.cs
@@ -1,34 +1,9 @@
-﻿using System;
+﻿using singleton1;
+using System;
 using System.Collections.Generic;
 
 namespace LoggerExample
 {
-    public class Logger
-    {
-        private static readonly Lazy<Logger> _instance =
-            new Lazy<Logger>(() => new Logger());
-
-        private readonly List<string> _messages;
-
-        public static Logger Instance => _instance.Value;
-
-        private Logger()
-        {
-            _messages = new List<string>();
-        }
-
-        public void Log(string message)
-        {
-            if (string.IsNullOrWhiteSpace(message))
-                throw new ArgumentException("Message cannot be empty", nameof(message));
-
-            _messages.Add(message);
-            Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
-        }
-
-        public IReadOnlyList<string> Messages => _messages.AsReadOnly();
-    }
-
     public class Program
     {
         public static void Main()
